8742736367066407652053b5d61cd8b9c6d1474a
Karin Lešnik <57890785+lesniikova@users.noreply.github.com>
2025-02-09 16:02:35 +0100

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  imports: [CommonModule]
+})
+export class AppComponent implements OnInit {
+  title = 'Tabela';
+  podatki: any[] = [];
+  loading = true;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get<any[]>('http://127.0.0.1:5000/table')
+      .subscribe({
+        next: (data) => {
+          this.podatki = data;
+          this.loading = false;
+        },
+        error: (err) => {
+          console.error('Napaka pri nalaganju podatkov:', err);
+          this.loading = false;
+        }
+      });
+  }
+}
